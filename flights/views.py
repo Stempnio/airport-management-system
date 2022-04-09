ce6119ffb51e29a2b3349d66c4516c541ceac11f
verduttio <bszwaja20@gmail.com>
2022-04-09 16:44:21 +0200

    login page template

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login_me(request):
+    return render(request, 'login.html', {'name': 'Userrito'})
