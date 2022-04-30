671d4d35555e25fb6e140e4201f3868b5efbc3f4
verduttio <bszwaja20@gmail.com>
2022-04-30 14:46:31 +0200

    clock fix: display constant format (add 0 when s/m/h is a one-digit number)

--- flights/static/general/scripts.js
+++ flights/static/general/scripts.js
@@ -1,15 +1,17 @@
-// function updateClock(id) {
-//     let now = new Date()
-//     let months = ['January', 'February', 'March', 'April',
-//         'May', 'June', 'July', 'August', 'September',
-//         'October', 'November', 'December'];
-//     let time = now.getHours() + ':' + now.getMinutes() + ":" + now.getSeconds()
-//
-//     let date = [now.getDate(),
-//                 months[now.getMonth()],
-//                 now.getFullYear()].join(' ');
-//
-//     document.getElementById(id).innerHTML = [date, time].join(' / ');
-//
-//     setTimeout(updateClock, 1000);
-// }
+function updateClock() {
+    let now = new Date()
+    let months = ['January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August', 'September',
+        'October', 'November', 'December'];
+    let time = String(now.getHours()).padStart(2, "0") + ':' + String(now.getMinutes()).padStart(2, "0") + ":" + String(now.getSeconds()).padStart(2, "0")
+
+    let date = [now.getDate(),
+        months[now.getMonth()],
+        now.getFullYear()].join(' ');
+
+    document.getElementById('time').innerHTML = [date, time].join(' / ');
+
+    setTimeout(updateClock, 1000);
+}
+
+updateClock(); // initial call
